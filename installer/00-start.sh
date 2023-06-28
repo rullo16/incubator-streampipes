9b9d937b2f1089da2245fd929ea14b4b14128495
root <root@gft-kyklos.duckdns.org>
2023-06-28 13:45:36 +0000

    fix

--- installer/00-start.sh
+++ installer/00-start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 mv -f ./SP-log.log ./SP-log.1
 #cat /dev/null > ./SP-log.log
-nohup ./cli/streampipes up 2>&1 1>./SP-log.log &
+#nohup ./cli/streampipes up 2>&1 1>./SP-log.log &
+./cli/streampipes up -d
